d5d981fecd9c443d7e82bbf498f3770fc4b02556
XOneFG <117354752+VsrStudio@users.noreply.github.com>
2025-07-10 00:57:27 +0700

    working on latest version 1.21.93

--- src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
+++ src/AkmalFairuz/MultiVersion/network/ProtocolConstants.php
@@ -10,19 +10,19 @@ class ProtocolConstants{
 
     public const PROTOCOL_1_21_93 = 819;
     public const PROTOCOL_1_21_90 = 818;
-	public const PROTOCOL_1_21_80 = 800;
-	public const PROTOCOL_1_21_70 = 786;
-	public const PROTOCOL_1_21_60 = 776;
-	public const PROTOCOL_1_21_50 = 766;
-	public const PROTOCOL_1_21_40 = 748;
-	public const PROTOCOL_1_21_30 = 729;
-	public const PROTOCOL_1_21_20 = 712;
-	public const PROTOCOL_1_21_2 = 686;
-	public const PROTOCOL_1_21_0 = 685;
-	public const PROTOCOL_1_20_80 = 671;
-	public const PROTOCOL_1_20_70 = 662;
-	public const PROTOCOL_1_20_60 = 649;
-	public const PROTOCOL_1_20_50 = 630;
+    public const PROTOCOL_1_21_80 = 800;
+    public const PROTOCOL_1_21_70 = 786;
+    public const PROTOCOL_1_21_60 = 776;
+    public const PROTOCOL_1_21_50 = 766;
+    public const PROTOCOL_1_21_40 = 748;
+    public const PROTOCOL_1_21_30 = 729;
+    public const PROTOCOL_1_21_20 = 712;
+    public const PROTOCOL_1_21_2 = 686;
+    public const PROTOCOL_1_21_0 = 685;
+    public const PROTOCOL_1_20_80 = 671;
+    public const PROTOCOL_1_20_70 = 662;
+    public const PROTOCOL_1_20_60 = 649;
+    public const PROTOCOL_1_20_50 = 630;
     public const BEDROCK_1_18_0 = 475;
     public const BEDROCK_1_17_40 = 471;
     public const BEDROCK_1_17_30 = 465;
@@ -34,18 +34,18 @@ class ProtocolConstants{
         self::PROTOCOL_1_21_93, => "1.21.93",
         self::PROTOCOL_1_21_90, => "1.21.90",
         self::PROTOCOL_1_21_80, => "1.21.80",
-	    self::PROTOCOL_1_21_70, => "1.21.70",
-		self::PROTOCOL_1_21_60, => "1.21.60",
-		self::PROTOCOL_1_21_50, => "1.21.50",
-		self::PROTOCOL_1_21_40, => "1.21.40",
-		self::PROTOCOL_1_21_30, => "1.21.30",
-		self::PROTOCOL_1_21_20, => "1.21.20",
-		self::PROTOCOL_1_21_2, => "1.21.2",
-		self::PROTOCOL_1_21_0, => "1.21.0",
-		self::PROTOCOL_1_20_80, => "1.20.80",
-		self::PROTOCOL_1_20_70, => "1.20.70",
-		self::PROTOCOL_1_20_60, => "1.20.60",
-		self::PROTOCOL_1_20_50, => "1.20.50",
+	self::PROTOCOL_1_21_70, => "1.21.70",
+	self::PROTOCOL_1_21_60, => "1.21.60",
+	self::PROTOCOL_1_21_50, => "1.21.50",
+	self::PROTOCOL_1_21_40, => "1.21.40",
+	self::PROTOCOL_1_21_30, => "1.21.30",
+	self::PROTOCOL_1_21_20, => "1.21.20",
+	self::PROTOCOL_1_21_2, => "1.21.2",
+	self::PROTOCOL_1_21_0, => "1.21.0",
+	self::PROTOCOL_1_20_80, => "1.20.80",
+	self::PROTOCOL_1_20_70, => "1.20.70",
+	self::PROTOCOL_1_20_60, => "1.20.60",
+	self::PROTOCOL_1_20_50, => "1.20.50",
         self::BEDROCK_1_18_0, => "1.18.0",
         self::BEDROCK_1_17_40, => "1.17.40",
         self::BEDROCK_1_17_30, => "1.17.30",
@@ -58,18 +58,18 @@ class ProtocolConstants{
     public const SUPPORTED_PROTOCOLS = [
         self::PROTOCOL_1_21_93,
         self::PROTOCOL_1_21_80,
-	    self::PROTOCOL_1_21_70,
-		self::PROTOCOL_1_21_60,
-		self::PROTOCOL_1_21_50,
-		self::PROTOCOL_1_21_40,
-		self::PROTOCOL_1_21_30,
-		self::PROTOCOL_1_21_20,
-		self::PROTOCOL_1_21_2,
-		self::PROTOCOL_1_21_0,
-		self::PROTOCOL_1_20_80,
-		self::PROTOCOL_1_20_70,
-		self::PROTOCOL_1_20_60,
-		self::PROTOCOL_1_20_50,
+	self::PROTOCOL_1_21_70,
+	self::PROTOCOL_1_21_60,
+	self::PROTOCOL_1_21_50,
+	self::PROTOCOL_1_21_40,
+	self::PROTOCOL_1_21_30,
+	self::PROTOCOL_1_21_20,
+	self::PROTOCOL_1_21_2,
+	self::PROTOCOL_1_21_0,
+	self::PROTOCOL_1_20_80,
+	self::PROTOCOL_1_20_70,
+	self::PROTOCOL_1_20_60,
+	self::PROTOCOL_1_20_50,
         self::BEDROCK_1_18_0,
         self::BEDROCK_1_17_40,
         self::BEDROCK_1_17_30,
